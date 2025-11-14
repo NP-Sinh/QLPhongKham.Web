ea0e079bcd46b40ea376ba54e92eb530202b95c3
NP-Sinh <npsinh6103@gmail.com>
2025-11-15 01:33:15 +0700

    update

--- src/app/views/vai-tro/vai-tro-list/vai-tro-list.ts
+++ src/app/views/vai-tro/vai-tro-list/vai-tro-list.ts
@@ -1,25 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, startWith, catchError } from 'rxjs/operators';
 import { VaiTroModel } from '../../../models/vaitro.model';
 import { VaiTroService } from '../../../services/VaiTroServices/vaitro.service';
+import { LoadingOverlay } from '../../../components/loading-overlay/loading-overlay';
+
+
+interface VaiTroState {
+  loading: boolean;
+  data: VaiTroModel[] | null;
+  error: any | null;
+}
 
 @Component({
   selector: 'app-vai-tro-list',
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingOverlay],
   templateUrl: './vai-tro-list.html',
   styleUrl: './vai-tro-list.css',
 })
 export class VaiTroList {
-  public vaiTros$!: Observable<VaiTroModel[]>;
-  constructor(private vaiTroService: VaiTroService) {};
 
-  ngOnInit(): void {
+  public state$!: Observable<VaiTroState>;
+
+  constructor(private vaiTroService: VaiTroService) {}
+
+  ngOnInit() {
     this.loadVaiTros();
   }
 
-  loadVaiTros(): void {
-    this.vaiTros$ = this.vaiTroService.getVaiTros();
+  loadVaiTros() {
+    this.state$ = this.vaiTroService.getVaiTros().pipe(
+      map((data) => ({ loading: false, data: data, error: null })),
+      startWith({ loading: true, data: null, error: null }),
+      catchError((err) =>  of({ loading: false, data: null, error: err })
+      )
+    );
   }
-
 }
